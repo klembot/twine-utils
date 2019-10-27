cd79b0787d42da2432afa57d0955805294f87a16
Chris Klimas <chris@twinery.org>
2019-10-26 21:43:35 -0400

    Update CLI

--- bin/entwee.js
+++ bin/entwee.js
@@ -1,24 +1,20 @@
 #!/usr/bin/env node
 const fs = require('fs');
 const path = require('path');
-const {Story, StoryFormat} = require('../dist/');
+const {Story} = require('../dist');
 const argv = require('yargs')
 	.usage('Usage: $0 <files to combine> [options]')
 	.alias('n', 'name')
 	.describe('name', 'Name to set on the generated file')
-	.alias('f', 'format')
-	.describe('format', 'Path to a Twine 2 story format to use')
+	.alias('o', 'sort')
+	.describe('sort', 'Sort passages by name')
 	.alias('s', 'start')
 	.describe('start', 'Name of the passage to set as start')
 	.describe('twee-version', 'Version of Twee spec to use')
 	.default('twee-version', 3)
-	.demand('format', 'A story format must be specified')
 	.demand(1)
 	.help().argv;
 
-const format = new StoryFormat(
-	fs.readFileSync(argv.format, {encoding: 'utf8'})
-);
 const story = new Story();
 
 argv._.forEach(srcFile => {
@@ -41,7 +37,7 @@ argv._.forEach(srcFile => {
 		case '.txt':
 		case '.twee':
 		case '.tw':
-			story.mergeTwee(src, argv.tweeVersion);
+			story.mergeTwee(src);
 			break;
 
 		default:
@@ -56,8 +52,22 @@ if (argv.name) {
 	story.attributes.name = argv.name;
 }
 
+if (argv.sort) {
+	story.passages.sort((a, b) => {
+		if (a.attributes.name < b.attributes.name) {
+			return -1;
+		}
+
+		if (b.attributes.name < a.attributes.name) {
+			return 1;
+		}
+
+		return 0;
+	});
+}
+
 if (argv.start) {
 	story.setStartByName(argv.start);
 }
 
-console.log(format.publish(story));
+console.log(story.toTwee(argv.tweeVersion));
