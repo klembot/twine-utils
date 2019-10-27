006237b5898b5cec334ef240f2e6b7449fd4be3b
Chris Klimas <chris@twinery.org>
2019-10-27 13:16:10 -0400

    Update files in dist/

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,5 @@
+import * as Path from './path';
+import Passage from './passage';
 import Story from './story';
 import StoryFormat from './story-format';
-export { Story, StoryFormat };
+export { Passage, Path, Story, StoryFormat };
