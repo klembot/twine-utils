0c4467a5fb4959eba0017ed4ec8f2d4385a256c1
Chris Klimas <hello@chrisklimas.com>
2023-07-17 20:32:29 -0400

    Add gitignore

--- dist/cjs/index.d.ts
+++
@@ -1,4 +0,0 @@
-export * as Path from './path';
-export { Passage } from './passage';
-export { Story } from './story';
-export { StoryFormat } from './story-format';
--- dist/cjs/path.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Returns a promise to an absolute path to the user's story directory. If it
- * can't be found, this rejects.
- */
-export declare function storyDirectory(): Promise<string>;
-/**
- * Synchronous version of storyDirectory().
- */
-export declare function storyDirectorySync(): string;
